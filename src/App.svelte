19760de0ac8606811bdbce65ea11dc8f48f51f82
Ruben van Nieuwpoort <ruben@orbisk.com>
2024-02-11 18:44:42 +0100

    Add dialog

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import Item from './components/Item.svelte';
 	import Fab, { Icon } from '@smui/fab';
+	import Button, { Label } from '@smui/button';
+	import Dialog, { Header, Content, Actions, InitialFocus } from '@smui/dialog';
+	import IconButton from '@smui/icon-button';
     import type { SyncPoint } from './types/SyncPoint';
 
     let content: SyncPoint[] = [
@@ -19,7 +22,9 @@
 			source: 'asdfsadfadsf',
 			active: true
 		},
-    ]
+    ];
+
+	let open = true;
 </script>
 
 <main>
@@ -36,6 +41,31 @@
 			</Fab>
 		</div>
 	</div>
+
+	<Dialog
+		bind:open
+		fullscreen
+		aria-labelledby="fullscreen-title"
+		aria-describedby="fullscreen-content"
+	>
+		<Header>
+			<h2 contenteditable="true">Hello there</h2>
+			<IconButton size="mini">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+			</IconButton>
+		</Header>
+		<Content id="fullscreen-content">
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+		</Content>
+		<Actions>
+			<Button action="reject">
+				<Label>Save</Label>
+			</Button>
+			<Button action="accept" defaultAction>
+				<Label>Cancel</Label>
+			</Button>
+		</Actions>
+	</Dialog>
 </main>
 
 <style>
