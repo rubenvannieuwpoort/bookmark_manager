73a565d5d38c60ae4c3bbbab0be5a8e8072fd4f0
Ruben van Nieuwpoort <ruben@orbisk.com>
2024-02-16 14:56:38 +0100

    Actually implement bookmark loading

--- src/browser-specific/bookmarks.ts
+++ src/browser-specific/bookmarks.ts
@@ -3,6 +3,16 @@ import type { Bookmark } from "../bookmarks";
 export async function setBookmarks(bookmarks: Bookmark[], path: string) {
     console.log(`importing bookmarks to "${path}":`);
     console.log(bookmarks);
+
+    chrome.bookmarks.create({ title: path, parentId: '1' }, (folder) => {
+        bookmarks.forEach((bookmark) => {
+            chrome.bookmarks.create({
+              parentId: folder.id,
+              title: bookmark.name,
+              url: bookmark.url,
+            });
+          });
+    });
 }
 
 export async function removeBookmarks(bookmarks: Bookmark[], path: string) {
