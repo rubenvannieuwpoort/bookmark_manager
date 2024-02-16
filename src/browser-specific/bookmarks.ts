d9c592a4d45a50fa9017c35d6dc3624af1a3885e
Ruben van Nieuwpoort <ruben@orbisk.com>
2024-02-16 15:37:25 +0100

    Use async/await instead of callback

--- src/browser-specific/bookmarks.ts
+++ src/browser-specific/bookmarks.ts
@@ -1,15 +1,13 @@
 import type { Bookmark } from "../bookmarks";
 
 export async function setBookmarks(bookmarks: Bookmark[], path: string) {
-	// TODO: handle error paths
-	// TODO: can we use async/await here?
-	chrome.bookmarks.create({ title: path, parentId: '1' }, (folder) => {
-		bookmarks.forEach((bookmark) => {
-			chrome.bookmarks.create({
-				parentId: folder.id,
-				title: bookmark.name,
-				url: bookmark.url,
-			});
+	// TODO: if a folder with this name already exists, that folder should be used
+	var folder = await chrome.bookmarks.create({ title: path, parentId: '1' });
+	bookmarks.forEach((bookmark) => {
+		chrome.bookmarks.create({
+			parentId: folder.id,
+			title: bookmark.name,
+			url: bookmark.url,
 		});
 	});
 }
